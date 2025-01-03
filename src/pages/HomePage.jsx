e2d2e9b48f38e1e5ee6e9b8b2b82a546f335d930
Ryuzaki <bpow2308@gmail.com>
2025-01-03 23:49:07 +0800

    vercel host

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,10 +5,10 @@ import useFetch from "@/hooks/useFetch";
 import AddPost from "@/components/AddPost";
 
 export default function HomePage() {
-  const { data, error, isFetching } = useFetch("PostTbl");
-  console.log([...data].reverse());
+  const { data: postData, error, isFetching } = useFetch("PostTbl");
+  console.log([...postData].reverse());
 
-  if (isFetching || error) {
+  if (isFetching) {
     return (
       <Page>
         <div className="flex flex-col items-center justify-center gap-2 p-2 px-6">
@@ -23,7 +23,7 @@ export default function HomePage() {
   return (
     <Page>
       <div className="flex flex-col items-center justify-center gap-2 p-2 px-6">
-        {[data].reverse().map((data) => (
+        {[...postData].reverse().map((data) => (
           <PostCard cardData={data} key={data.id} />
         ))}
       </div>
--- src/pages/NotPage.jsx
+++ src/pages/NotPage.jsx
@@ -1,3 +1,11 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+
 export default function NotPage() {
+  const navgate = useNavigate();
+  useEffect(() => {
+    console.log("navigate");
+    navgate("/", { replace: true });
+  }, [navgate]);
   return <div>NotPage</div>;
 }
