e5d5ed92222b5bc809a63b820052d3dfb7f73549
Ryuzaki <bpow2308@gmail.com>
2025-01-16 01:43:28 +0800

    Fix Home Route v3

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,21 +1,24 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router";
+import {useEffect} from 'react';
+import {useNavigate} from 'react-router';
 
 export default function LandingPage() {
   const navigate = useNavigate();
   useEffect(() => {
-    const isOldUser = localStorage.getItem("oldUser");
+    const fetchLocal = async () => {
+      const isOldUser = await localStorage.getItem('oldUser');
 
-    if (isOldUser) {
-      navigate("/home", { replace: true });
-    }
+      if (isOldUser) {
+        navigate('/home', {replace: true});
+      }
+    };
+    fetchLocal();
   }, [navigate]);
   const onClickProceed = () => {
-    localStorage.setItem("oldUser", true);
-    navigate("/home", { replace: true });
+    localStorage.setItem('oldUser', true);
+    navigate('/home', {replace: true});
   };
   return (
-    <div className="flex h-screen flex-col gap-5 bg-red-500">
+    <div className='flex h-screen flex-col gap-5 bg-red-500'>
       <span> LandingPage</span>
       <button onClick={onClickProceed}>Proceed</button>
     </div>
